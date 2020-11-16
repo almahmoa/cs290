c6aac1aeca724c71cc836189ebf363bcc1e70c74
almahmoa <55867275+almahmoa@users.noreply.github.com>
2020-11-15 18:18:33 -0800

    Update checker.js

--- checker.js
+++ checker.js
@@ -4,12 +4,12 @@ var bodyParser = require('body-parser');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 7181);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   var qParams = [];
